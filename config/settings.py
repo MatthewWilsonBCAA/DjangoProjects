d06fa8f0ccc2757b1796d27a04eab7feee08dcff
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-01-04 15:51:21 -0600

    adding debug readouts

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,16 @@ from environs import Env
 env = Env()
 env.read_env()
 
+import sentry_sdk
+
+sentry_sdk.init(
+    "https://examplePublicKey@o0.ingest.sentry.io/0",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
