ff30aae62a51e78b5b2848ec2c19c1205e1c5d5e
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-01-15 11:34:19 -0600

    fixed homepage bug

--- blog/views.py
+++ blog/views.py
@@ -21,20 +21,21 @@ class UserListView(ListView):
 class OrderingByQueryParams:
     def get_ordering(self):
         sort_by_n = self.request.GET.get("sortby")
-        sk = list(sort_by_n)
-        seen = []
-        result = []
-        for i in sk:
-            if i == "t" or i == "o" or i == "p" or i == "n" or i == "e" or i == "w":
-                if "".join(result) == "new" or "".join(result) == "top":
-                    break
-                if not i in seen:
-                    seen.append(i)
-                    result.append(i)
-                    continue
-        sort_by_n = "".join(result)
-        # sort_by_n = sort_by_n.strip("/?page= ")
-        print(sort_by_n)
+        try:
+            sk = list(sort_by_n)
+            seen = []
+            result = []
+            for i in sk:
+                if i == "t" or i == "o" or i == "p" or i == "n" or i == "e" or i == "w":
+                    if "".join(result) == "new" or "".join(result) == "top":
+                        break
+                    if not i in seen:
+                        seen.append(i)
+                        result.append(i)
+                        continue
+            sort_by_n = "".join(result)
+        except:
+            sort_by_n = "new"
         if sort_by_n == "top":
             return "-num_votes"
         else:
@@ -62,19 +63,22 @@ class BlogListView(OrderingByQueryParams, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         sort_by_n = self.request.GET.get("sortby")
-        sk = list(sort_by_n)
-        seen = []
-        result = []
-        for i in sk:
-            if i == "t" or i == "o" or i == "p" or i == "n" or i == "e" or i == "w":
-                if "".join(result) == "new" or "".join(result) == "top":
-                    break
-                if not i in seen:
-                    seen.append(i)
-                    result.append(i)
-                    continue
-        sort_by_n = "".join(result)
-        context["cursort"] = sort_by_n
+        try:
+            sk = list(sort_by_n)
+            seen = []
+            result = []
+            for i in sk:
+                if i == "t" or i == "o" or i == "p" or i == "n" or i == "e" or i == "w":
+                    if "".join(result) == "new" or "".join(result) == "top":
+                        break
+                    if not i in seen:
+                        seen.append(i)
+                        result.append(i)
+                        continue
+            sort_by_n = "".join(result)
+            context["cursort"] = sort_by_n
+        except:
+            context["corsort"] = "new"
         return context
 
 
